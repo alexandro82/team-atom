5c79c7c1e0aff18aa2d0a0e6f93377f14b4d2971
alexandro82 <alexandro82@gmail.com>
2012-12-01 15:36:39 -0400

    Eliminados archivos de modelo de datos innecesarios

--- model/modificaciones_modelo.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `municipio` CHANGE `municipio_codigo` `municipio_codigo_ine` VARCHAR( 45 ) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL ;
-ALTER TABLE `municipio` ADD `municipio_codigo_ine_anterior` VARCHAR( 30 ) NOT NULL AFTER `municipio_codigo_ine` ,
-ADD `municipio_codigo_economia_finanzas` VARCHAR( 30 ) NOT NULL AFTER `municipio_codigo_ine_anterior` ;
