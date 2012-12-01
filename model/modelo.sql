cf57477ad1dc105c96235549f5b7ff37d4a44433
alexandro82 <alexandro82@gmail.com>
2012-12-01 15:35:33 -0400

    Modelo de datos modificado: tabla municipios actualizada con campos de indices

--- model/modelo.sql
+++ model/modelo.sql
@@ -1,65 +1,95 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 3.5.4
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 01-12-2012 a las 17:57:27
+-- Versión del servidor: 5.5.28-log
+-- Versión de PHP: 5.4.9
 
-CREATE SCHEMA IF NOT EXISTS `atom` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `atom` ;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Table `atom`.`municipio`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `atom`.`municipio` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `municipio_nombre` VARCHAR(45) NULL ,
-  `municipio_departamento` VARCHAR(45) NULL ,
-  `municipio_longitud` VARCHAR(45) NULL ,
-  `municipio_latitud` VARCHAR(45) NULL ,
-  `municipio_categoria` VARCHAR(45) NULL ,
-  `municipio_codigo` VARCHAR(45) NULL ,
-  `municipio_estado` VARCHAR(45) NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+--
+-- Base de datos: `atom`
+--
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `atom`.`indicador`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `atom`.`indicador` (
-  `id` INT NOT NULL ,
-  `indicador_descripcion` VARCHAR(45) NULL ,
-  `indicador_tipo` VARCHAR(45) NULL ,
-  `indicador_estado` VARCHAR(45) NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+--
+-- Estructura de tabla para la tabla `indicador`
+--
 
+CREATE TABLE IF NOT EXISTS `indicador` (
+  `id` int(11) NOT NULL,
+  `indicador_descripcion` varchar(150) DEFAULT NULL,
+  `indicador_tipo` varchar(45) DEFAULT NULL,
+  `indicador_estado` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- -----------------------------------------------------
--- Table `atom`.`indice`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `atom`.`indice` (
-  `id` INT NOT NULL ,
-  `indice_gestion` VARCHAR(45) NULL ,
-  `indice_valor` VARCHAR(45) NULL ,
-  `municipio_id` INT NOT NULL ,
-  `indicador_id` INT NOT NULL ,
-  `indice_estado` VARCHAR(45) NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_indice_municipio_idx` (`municipio_id` ASC) ,
-  INDEX `fk_indice_indicador1_idx` (`indicador_id` ASC) ,
-  CONSTRAINT `fk_indice_municipio`
-    FOREIGN KEY (`municipio_id` )
-    REFERENCES `atom`.`municipio` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_indice_indicador1`
-    FOREIGN KEY (`indicador_id` )
-    REFERENCES `atom`.`indicador` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Volcado de datos para la tabla `indicador`
+--
+
+INSERT INTO `indicador` (`id`, `indicador_descripcion`, `indicador_tipo`, `indicador_estado`) VALUES
+(1, 'EJECUCIÓN DE RECURSOS', 'SUBINDICE', 'ACTIVO'),
+(2, 'EJECUCION DEL GASTO', 'SUBINDICE', 'ACTIVO'),
+(3, 'EFICIENCIA EN LA EJECUCIÓN DE RECURSOS Y GASTOS', 'INDICADOR', 'ACTIVO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `indice`
+--
+
+CREATE TABLE IF NOT EXISTS `indice` (
+  `id` int(11) NOT NULL,
+  `indice_gestion` int(4) DEFAULT NULL,
+  `indice_valor` double DEFAULT NULL,
+  `municipio_id` int(11) NOT NULL,
+  `indicador_id` int(11) NOT NULL,
+  `indice_estado` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_indice_municipio_idx` (`municipio_id`),
+  KEY `fk_indice_indicador1_idx` (`indicador_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
+--
+-- Estructura de tabla para la tabla `municipio`
+--
+
+CREATE TABLE IF NOT EXISTS `municipio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `municipio_nombre` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_departamento` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_longitud` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_latitud` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_categoria` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_codigo_ine` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `municipio_codigo_ine_anterior` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `municipio_codigo_economia_finanzas` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `municipio_estado` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=10 ;
+
+--
+-- Volcado de datos para la tabla `municipio`
+--
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `indice`
+--
+ALTER TABLE `indice`
+  ADD CONSTRAINT `fk_indice_municipio` FOREIGN KEY (`municipio_id`) REFERENCES `municipio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_indice_indicador1` FOREIGN KEY (`indicador_id`) REFERENCES `indicador` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
