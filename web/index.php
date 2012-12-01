9db1f5db7dff45fc176aaac8e3ecb58138fcd5a1
wchorolque <wchorolque@gmail.com>
2012-12-01 12:00:42 -0400

    agregado index.php y assets

--- web/index.php
+++ web/index.php
@@ -1,3 +1,102 @@
-<?php
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+<head>
+<meta charset="utf-8">
+<title>Team - Atom</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<script type="text/javascript" src="assets/js/modernizr-2.6.1.js"></script>
 
-echo "hello team";
+<!-- Le styles -->
+<link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+<link href="assets/css/start/jquery-ui-1.8.23.custom.css" rel="stylesheet">
+<link href="assets/css/modal_spinner.css" rel="stylesheet">
+<style type="text/css">
+body {
+    padding-top: 60px;
+    padding-bottom: 40px;
+}
+.span10 h4 {
+    text-align: justify;
+}
+h4 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+</style>
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+
+<!-- Le fav and touch icons -->
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+</head>
+
+<body>
+<!-- BODY -->
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner">
+<div class="container">
+<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</a>
+<a class="brand" href="index.php">DAL-2012</a>
+<div class="nav-collapse">
+<ul class="nav">
+<li class="active"><a href="index.php">Menu 01</a></li>
+<li><a href="reportes/index.php">Menu 02</a></li>
+</ul>
+</div><!--/.nav-collapse -->
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row-fluid">
+<div class="forms">
+</div>
+</div>
+<hr>
+<footer>
+<p style="text-align:center">TEAM-ATOM 2012</p>
+</footer>
+
+</div> <!-- /container -->
+
+<div class="modal-spinner" id="modal_spinner">
+<img src="assets/img/loading.gif" alt="Cargando..." />
+</div>
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery-1.8.0.min.js"></script>
+<script src="assets/js/jquery-ui-1.8.23.min.js"></script>
+<script src="assets/js/jquery.ui.datepicker-es.js"></script>
+<script src="assets/js/bootstrap-button.js"></script>
+<script src="assets/js/bootstrap-modal.js"></script>
+<!--
+<script src="assets/js/bootstrap-transition.js"></script>
+<script src="assets/js/bootstrap-alert.js"></script>
+<script src="assets/js/bootstrap-dropdown.js"></script>
+<script src="assets/js/bootstrap-scrollspy.js"></script>
+<script src="assets/js/bootstrap-tab.js"></script>
+<script src="assets/js/bootstrap-tooltip.js"></script>
+<script src="assets/js/bootstrap-popover.js"></script>
+<script src="assets/js/bootstrap-collapse.js"></script>
+<script src="assets/js/bootstrap-carousel.js"></script>
+<script src="assets/js/bootstrap-typeahead.js"></script>
+-->
+<!-- END BODY -->
+</body>
+</html>
+
+
