8cc79455ba8304c73d0e118b08c51cb707a72b5e
wchorolque <wchorolque@gmail.com>
2012-12-02 09:09:07 -0400

    agregado sidebar al diagrama de dispersion

--- web/dispersion/index.php
+++ web/dispersion/index.php
@@ -61,7 +61,32 @@ h4 {
 </div>
 
 <div class="container-fluid">
-<div class="row-fluid">
+    <div class="row-fluid">
+        <div class="span3">
+            <div class="well sidebar-nav">
+                <ul class="nav nav-list">
+                    <li class="nav-header">Sidebar</li>
+                    <li class="active"><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+
+                    <li class="nav-header">Sidebar</li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+
+                    <li><a href="#">Link</a></li>
+                    <li class="nav-header">Sidebar</li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+
+            </div><!--/.well -->
+        </div><!--/span-->
 <div class="forms">
 <div id="canvas">
 </div>
